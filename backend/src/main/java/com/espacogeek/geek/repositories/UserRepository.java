7f9591e525d6563f7843e7d52d0f00c86e52e3ca
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-15 12:53:22 -0300

    feat: added findByUsername

--- backend/src/main/java/com/espacogeek/geek/repositories/UserRepository.java
+++ backend/src/main/java/com/espacogeek/geek/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-    
+    User findByUsername(String username);
 }
