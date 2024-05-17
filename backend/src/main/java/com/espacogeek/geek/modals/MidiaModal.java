6a5d93f40a1ed567893d531ab77f6a2f8716ce8c
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-17 13:58:31 -0300

    feat: MidiaModal finished

--- backend/src/main/java/com/espacogeek/geek/modals/MidiaModal.java
+++ backend/src/main/java/com/espacogeek/geek/modals/MidiaModal.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -16,4 +18,21 @@ public class MidiaModal {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_midia")
     private Integer id;
+
+    @Column(name = "name_midia")
+    private String name;
+
+    @Column(name = "episode_count")
+    private Integer episode;
+
+    @Column(name = "episode_lenght_in_minutes")
+    private Integer episodeLenght;
+
+    @Column(name = "about")
+    private String about;
+
+    @Column(name = "path_cover")
+    private String pathCover;
+
+    //TODO: category relationship
 }
