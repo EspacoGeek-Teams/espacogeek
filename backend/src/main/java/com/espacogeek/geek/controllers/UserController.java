6da26c7fadc8ce27af4c2cc283f60abf5fb29e57
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-23 19:02:47 -0300

    feat: authentication

--- backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
+++ backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
@@ -1,5 +1,22 @@
 package com.espacogeek.geek.controllers;
 
-public class UserController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.espacogeek.geek.modals.UserModal;
+import com.espacogeek.geek.repositories.UserRepository;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @QueryMapping
+    public Optional<UserModal> userByUsername(@Argument String username) {
+        return userRepository.findByUsernameContains(username);
+    }
 }
--- backend/src/main/java/com/espacogeek/geek/repositories/UserRepository.java
+++ backend/src/main/java/com/espacogeek/geek/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.espacogeek.geek.repositories;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.espacogeek.geek.modals.UserModal;
 
 public interface UserRepository extends JpaRepository<UserModal, Integer>{
-    List<UserModal> findByUsername(UserModal id);
+    Optional<UserModal> findByUsernameContains(String id);
 }
