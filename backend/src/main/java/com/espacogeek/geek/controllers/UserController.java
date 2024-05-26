e089b70212dc5ec978e3eeedc10d7558ad378ed1
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-26 15:20:54 -0300

    feat: adding CRUD to User

--- backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
+++ backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.espacogeek.geek.controllers;
 
+import java.beans.Beans;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
+import com.espacogeek.geek.dto.UserInput;
+import com.espacogeek.geek.exception.GenericExeption;
 import com.espacogeek.geek.modals.UserModal;
 import com.espacogeek.geek.services.User.UserService;
 
@@ -34,9 +39,9 @@ public class UserController {
     }
 
     @MutationMapping
-    public UserModal registerUser(@Argument String username, @Argument String email, @Argument String password) {
-        var passwordCrypted = BCrypt.withDefaults().hash(12, password.toCharArray()); // Encrypt password
-        UserModal user = new UserModal(null, username, email, passwordCrypted);
+    public UserModal registerUser(@Argument UserInput userInput) {
+        var passwordCrypted = BCrypt.withDefaults().hash(12, userInput.password().toCharArray());
+        UserModal user = new UserModal(null, userInput.username(), userInput.email(), passwordCrypted);
         return userService.save(user);
     }
 }
---
+++ backend/src/main/java/com/espacogeek/geek/dto/UserInput.java
@@ -0,0 +1,3 @@
+package com.espacogeek.geek.dto;
+
+public record UserInput(String username, String email, String password) {}
