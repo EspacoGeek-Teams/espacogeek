6a9eccfb7744eb7ef8e30fa28bf92c2b622a7c66
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-28 22:48:34 -0300

    refractor: unificated methods

--- backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
+++ backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
@@ -9,14 +9,18 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 
-import com.espacogeek.geek.dto.UserInput;
 import com.espacogeek.geek.exception.GenericExeption;
 import com.espacogeek.geek.modals.UserModal;
 import com.espacogeek.geek.services.User.UserService;
+import com.espacogeek.geek.types.NewUser;
+import com.espacogeek.geek.types.UserInput;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.annotation.security.PermitAll;
 
 @Controller
 public class UserController {
@@ -24,24 +28,14 @@ public class UserController {
     private UserService userService;
 
     @QueryMapping
-    public Optional<UserModal> findUserByUsername(@Argument String username) {
-        return userService.findByUsernameContains(username);
-    }
-
-    @QueryMapping
-    public Optional<UserModal> findUserById(@Argument Integer id) {
-        return userService.findById(id);
-    }
-
-    @QueryMapping
-    public Optional<UserModal> findUserByEmail(@Argument String email) {
-        return userService.findByEmail(email);
+    public Optional<UserModal> findUser(@Argument(name = "filter") UserInput userInput) {
+        return userService.findByUsernameContains(userInput.getUsername());
     }
 
     @MutationMapping
-    public UserModal registerUser(@Argument UserInput userInput) {
-        var passwordCrypted = BCrypt.withDefaults().hash(12, userInput.password().toCharArray());
-        UserModal user = new UserModal(null, userInput.username(), userInput.email(), passwordCrypted);
+    public UserModal registerUser(@Argument(name = "newUser") NewUser newUser) {
+        var passwordCrypted = BCrypt.withDefaults().hash(12, newUser.getPassword().toCharArray());
+        UserModal user = new UserModal(null, newUser.getUsername(), newUser.getEmail(), passwordCrypted);
         return userService.save(user);
     }
 }
