c82ea992827cc74d5402276c012420c3b079b3ae
vitorhugo1207 <vitorhugoalvesferreira@gmail>
2024-05-28 22:46:49 -0300

    feat: adding security

---
+++ backend/src/main/java/com/espacogeek/geek/security/JwtGenerator.java
@@ -0,0 +1,10 @@
+package com.espacogeek.geek.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtGenerator {
+    public String generate(Jwt jwt){
+
+    }
+}
