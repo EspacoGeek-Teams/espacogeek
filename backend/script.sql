4069155103e8facc8cb85d4180da6513fd5ee715
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-06-13 12:36:47 -0300

    feat: add sql script with data that need to seed while doesn't have a properly seeder

---
+++ backend/script.sql
@@ -0,0 +1,15 @@
+INSERT INTO ESPACOGEEKDB.MEDIA_CATEGORIES VALUES (
+    1,
+    "TVSerie"
+);
+
+INSERT INTO ESPACOGEEKDB.API_KEYS VALUE (
+    1,
+    "",
+    "The Movie Database"
+);
+
+INSERT INTO ESPACOGEEKDB.TYPE_REFERENCE VALUE (
+    1,
+    "The Movie Database"
+);
