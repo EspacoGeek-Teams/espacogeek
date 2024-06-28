2c64be115ab8f23d52c08999140df8e005cf8763
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-06-27 21:59:59 -0300

    feat: add index to media name

--- backend/src/main/java/com/espacogeek/geek/models/MediaModel.java
+++ backend/src/main/java/com/espacogeek/geek/models/MediaModel.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
@@ -25,7 +26,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "medias")
+@Table(name = "medias", indexes = {@Index(name = "idx_name_media", columnList = "name", unique = true)})
 @Getter
 @Setter
 @AllArgsConstructor
