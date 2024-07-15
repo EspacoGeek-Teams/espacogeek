4ac451f736846255a44ffb19c931f13477fd9b87
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-15 13:56:52 -0300

    feat: add genres to database

--- backend/script.sql
+++ backend/script.sql
@@ -24,3 +24,30 @@ INSERT INTO ESPACOGEEKDB.TYPE_REFERENCE VALUE (
     3,
     "IMDb"
 );
+
+INSERT INTO espacogeekdb.genres (id_genre, name_genre)
+VALUES
+    (1,'Action'),
+    (2,'Adventure'),
+    (3,'Animation'),
+    (4,'Comedy'),
+    (5,'Crime'),
+    (6,'Documentary'),
+    (7,'Drama'),
+    (8,'Family'),
+    (9,'Fantasy'),
+    (10,'History'),
+    (11,'Horror'),
+    (12,'Musical'),
+    (13,'Mystery'),
+    (14,'Romance'),
+    (15,'Sci-Fi (Science Fiction)'),
+    (16,'TV Movie'),
+    (17,'Thriller'),
+    (18,'War'),
+    (19,'Western'),
+    (20,'Kids'),
+    (21,'News'),
+    (22,'Reality'),
+    (23,'Soap'),
+    (24,'Talk');
