9c498b1028e1f8f31bf579cd4da692aeaa58fa81
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-18 01:50:01 -0300

    feat: elaboration of query

--- backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
+++ backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
@@ -9,5 +9,7 @@ import com.espacogeek.geek.models.MediaModel;
 
 @Repository
 public interface MediaRepository extends JpaRepository<MediaModel, Integer> {
-    List<MediaModel> findSerieByIdOrNameAndMediaCategory(Integer id, String name, MediaCategoryModel category);
+    // SELECT DISTINCT * FROM medias m INNER JOIN alternative_titles a ON m.id_media = a.id_midia INNER JOIN media_categories c ON c.id_media_category = 1 WHERE m.id_media = 159191 OR m.name_media LIKE "%Stranger Things%" OR a.name_title LIKE "%Coisas Estranhas%";
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.query-methods.at-query
+    List<MediaModel> findSerieByIdOrNameOrAlternativeTitleAndMediaCategory(Integer id, String name, String alternativeTitle, MediaCategoryModel category);
 }
