c43fa7daf852cd2121c9ce55b564b30fb5451267
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-23 17:09:11 -0300

    feat: starting write test to Media

--- backend/src/test/java/com/espacogeek/geek/GeekApplicationTests.java
+++ backend/src/test/java/com/espacogeek/geek/GeekApplicationTests.java
@@ -3,11 +3,9 @@ package com.espacogeek.geek;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.espacogeek.geek.User.UserTests;
+
 @SpringBootTest
 class GeekApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    
 }
---
+++ backend/src/test/java/com/espacogeek/geek/Media/MediaTest.java
@@ -0,0 +1,8 @@
+package com.espacogeek.geek.Media;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MediaTest {
+    
+}
