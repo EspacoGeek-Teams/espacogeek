43220a8f73a7d14dd08d6618ab3733b4549f6472
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-23 12:47:54 -0300

    refractor: rename query name

--- backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
+++ backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
@@ -11,6 +11,7 @@ import com.espacogeek.geek.models.MediaModel;
 
 @Repository
 public interface MediaRepository extends JpaRepository<MediaModel, Integer> {
+    
     @Query("SELECT m FROM MediaModel m INNER JOIN AlternativeTitleModel a ON a MEMBER OF m.alternativeTitles INNER JOIN MediaCategoryModel c ON c = :category INNER JOIN ExternalReferenceModel e ON e.media = m WHERE m.id = :id OR m.name LIKE CONCAT('%',:name,'%') OR a.name LIKE CONCAT('%',:alternativeTitle,'%')")
-    List<MediaModel> findSerieByIdOrNameOrAlternativeTitleAndMediaCategory(@Param("id") Integer id, @Param("name") String name, @Param("alternativeTitle") String alternativeTitle, @Param("category") MediaCategoryModel category);
+    List<MediaModel> findMediaByIdOrNameOrAlternativeTitleAndMediaCategory(@Param("id") Integer id, @Param("name") String name, @Param("alternativeTitle") String alternativeTitle, @Param("category") MediaCategoryModel category);
 }
