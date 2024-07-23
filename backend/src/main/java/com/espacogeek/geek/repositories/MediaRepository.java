101d603a270266a3776e4c70c3b71bbfb9c66330
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-23 12:23:55 -0300

    feat: creating query

--- backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
+++ backend/src/main/java/com/espacogeek/geek/repositories/MediaRepository.java
@@ -3,6 +3,7 @@ package com.espacogeek.geek.repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.espacogeek.geek.models.MediaCategoryModel;
@@ -10,6 +11,6 @@ import com.espacogeek.geek.models.MediaModel;
 
 @Repository
 public interface MediaRepository extends JpaRepository<MediaModel, Integer> {
-    @Query("SELECT DISTINCT m, a, c, e FROM MediaModel m INNER JOIN AlternativeTitleModel a ON m.alternativeTitles = a INNER JOIN MediaCategoryModel c ON c = ?4 INNER JOIN ExternalReference e ON e.media = m WHERE m.id = ?1 OR m.name LIKE '%?2%' OR a.name LIKE %?3%")
-    List<MediaModel> findSerieByIdOrNameOrAlternativeTitleAndMediaCategory(Integer id, String name, String alternativeTitle, MediaCategoryModel category);
+    @Query("SELECT DISTINCT m, a, c, e FROM MediaModel m INNER JOIN AlternativeTitleModel a ON m.alternativeTitles title of a INNER JOIN MediaCategoryModel c ON c = :category INNER JOIN ExternalReference e ON e.media = m WHERE m.id = :id OR m.name LIKE CONCAT('%',:name,'%') OR a.name LIKE CONCAT('%',:alternativeTitle,'%')")
+    List<MediaModel> findSerieByIdOrNameOrAlternativeTitleAndMediaCategory(@Param("id") Integer id, @Param("name") String name, @Param("alternativeTitle") String alternativeTitle, @Param("category") MediaCategoryModel category);
 }
