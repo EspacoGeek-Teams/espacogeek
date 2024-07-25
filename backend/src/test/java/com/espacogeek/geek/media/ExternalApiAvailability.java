0a93effd1500933bd9c69170e19223502789358f
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-25 08:41:09 -0300

    feat: handle connection exeception

--- backend/src/test/java/com/espacogeek/geek/media/ExternalApiAvailability.java
+++ backend/src/test/java/com/espacogeek/geek/media/ExternalApiAvailability.java
@@ -2,6 +2,7 @@ package com.espacogeek.geek.media;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.rmi.ConnectException;
 import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,9 +18,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.espacogeek.geek.data.api.MediaApi;
+import com.espacogeek.geek.models.ApiKeyModel;
 import com.espacogeek.geek.services.ApiKeyService;
 
 import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.model.core.responses.TmdbResponseException;
 import info.movito.themoviedbapi.tools.TmdbException;
 
 @Nested
@@ -32,8 +35,16 @@ public class ExternalApiAvailability {
     private ApiKeyService apiKeyService;
 
     @Test
-    void tmdb() throws TmdbException {
-        var key = apiKeyService.findById(MediaApi.TMDB_API_KEY_ID).orElseThrow(() -> new NoSuchElementException("API key not found in database."));
-        assertEquals(new TmdbApi(key.getKey()).getAuthentication().validateKey().getSuccess(), true);
+    void tmdb() throws TmdbException, ConnectException {
+        boolean status = false;
+        ApiKeyModel key = apiKeyService.findById(MediaApi.TMDB_API_KEY_ID).orElseThrow(() -> new NoSuchElementException("API key not found in database."));
+        
+        try {
+            status = new TmdbApi(key.getKey()).getAuthentication().validateKey().getSuccess();
+        } catch (TmdbResponseException e) {
+            throw new ConnectException("Failed to connect The Movie Database servers.");
+        }
+
+        assertEquals(status, true);
     }
 }
