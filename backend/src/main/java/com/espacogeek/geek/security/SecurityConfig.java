089c8da636256c52760770adaefdb04f5d1e3d47
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-27 03:17:39 -0300

    feat: enable Security

---
+++ backend/src/main/java/com/espacogeek/geek/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.espacogeek.geek.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    
+}
