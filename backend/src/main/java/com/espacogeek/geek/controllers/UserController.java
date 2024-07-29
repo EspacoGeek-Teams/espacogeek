b5c844c345fd6d5d0d1036f9a900ffa6d2b94d90
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-29 17:46:29 -0300

    refractor: transfer authController methods to UserController

--- backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
+++ backend/src/main/java/com/espacogeek/geek/controllers/UserController.java
@@ -10,12 +10,14 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import com.espacogeek.geek.models.UserModel;
 import com.espacogeek.geek.services.UserService;
+import com.espacogeek.geek.types.NewUser;
 import com.espacogeek.geek.types.UserInput;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-
 
 @Controller
 public class UserController {
@@ -23,15 +25,26 @@ public class UserController {
     private UserService userService;
 
     @QueryMapping
-    public List<Optional<UserModel>> findUser(@Argument(name = "filter") UserInput userInput) {
-        return userService.findByIdOrUsernameContainsOrEmail(userInput.getId(), userInput.getUsername(), userInput.getEmail());
+    public List<UserModel> findUser(Integer id, String username, String email) {
+        return userService.findByIdOrUsernameContainsOrEmail(id, username, email);
+    }
+    
+    @MutationMapping(name = "create")
+    public String createUser(@Argument(name = "credentials") NewUser newUser) {
+        var passwordCrypt = BCrypt.withDefaults().hash(12, newUser.password().toCharArray()); 
+        var user = new UserModel(null, newUser.username(), newUser.email(), passwordCrypt, null);
+
+        userService.save(user);
+
+        return HttpStatus.CREATED.toString();
     }
 
     @MutationMapping(name = "editPassword")
     public String editPasswordUser(@Argument String actualPassword, @Argument String newPassword, @ContextValue String userId) {
-        var userLoged = userService.findById(Integer.valueOf(userId)).get();  // * @AbigailGeovana pega o usuario que foi definido no contexto e procura por ele 
+        var userLoged = userService.findById(Integer.valueOf(userId)).get();
         var resultPassword = BCrypt.verifyer().verify(actualPassword.toCharArray(), userLoged.getPassword()).verified;
-        if (resultPassword) { // * @AbigailGeovana checa a senha
+
+        if (resultPassword) {
             userLoged.setPassword(BCrypt.withDefaults().hash(12, newPassword.toCharArray()));
             userService.save(userLoged);
             return HttpStatus.OK.toString();
@@ -39,10 +52,11 @@ public class UserController {
         return HttpStatus.UNAUTHORIZED.toString();
     }
 
-    @MutationMapping()
+    @MutationMapping(name = "delete")
     public String deleteUser(@Argument String password, @ContextValue String userId) {
-        var userLoged = userService.findById(Integer.valueOf(userId)).get(); // * @AbigailGeovana pega o usuario que foi definido no contexto e procura por ele 
-        var resultPassword = BCrypt.verifyer().verify(password.toCharArray(), userLoged.getPassword()).verified; // * @AbigailGeovana checa a senha
+        var userLoged = userService.findById(Integer.valueOf(userId)).get(); 
+        var resultPassword = BCrypt.verifyer().verify(password.toCharArray(), userLoged.getPassword()).verified;
+        
         if (resultPassword) {
             userService.deleteById(Integer.valueOf(userId));
             return HttpStatus.OK.toString();
