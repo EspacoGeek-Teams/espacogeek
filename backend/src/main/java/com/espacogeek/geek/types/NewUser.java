5e149be6cde86be2816846be230539f4279fc07c
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-07-29 19:51:33 -0300

    refractor: remove Deprecated tag

--- backend/src/main/java/com/espacogeek/geek/types/NewUser.java
+++ backend/src/main/java/com/espacogeek/geek/types/NewUser.java
@@ -3,6 +3,5 @@ package com.espacogeek.geek.types;
 /**
  * Record class used in POST REST to create a new user
  */
-@Deprecated
 public record NewUser(String username, String email, String password) {
 }
