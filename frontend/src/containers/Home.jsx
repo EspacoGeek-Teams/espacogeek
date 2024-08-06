d49d560d1bada17b3177c9f0fdfd1e3880bbaa6c
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-08-06 02:28:36 -0300

    feat: building home and topbar

---
+++ frontend/src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TopBar from "../components/TopBar";
+import './star.css';
+
+function Home() {
+    return (
+        <>
+            <TopBar />
+            <div id="stars1"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+        </>
+    );
+}
+
+export default Home;
