4b5b6729c69dd7ca09f4dd8ac3b66e3168dc823e
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-08-12 00:53:23 -0300

    feat: CloseButton

--- frontend/src/components/topBar/SearchBar.jsx
+++ frontend/src/components/topBar/SearchBar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import CloseButton from 'react-bootstrap/CloseButton';
 
 function SearchBar() {
     return (
         <>
+            <div className="container absolute z-50">
+                <CloseButton onClick={() => setSearchComponent(false)} />
+            </div>
             <div className="absolute z-40 w-screen h-screen right-0 top-0 backdrop-blur-sm bg-blue-900 bg-opacity-10"></div>
         </>
     );
