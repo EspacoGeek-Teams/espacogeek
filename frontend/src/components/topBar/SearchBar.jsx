12b1427d1c7d28d60acd4b1ee56894df6e2ee81f
AbigailPedreira <abigailpedreira6@gmail.com>
2024-08-13 18:34:38 -0300

    comentário

--- frontend/src/components/topBar/SearchBar.jsx
+++ frontend/src/components/topBar/SearchBar.jsx
@@ -5,7 +5,9 @@ function SearchBar() {
     return (
         <>
             <div className="container absolute z-50">
-                <CloseButton onClick={() => setSearchComponent(false)} />
+                <CloseButton
+                    // onClick={() => setSearchComponent(false)}
+                />
             </div>
             <div className="absolute z-40 w-screen h-screen right-0 top-0 backdrop-blur-sm bg-blue-900 bg-opacity-10"></div>
         </>
