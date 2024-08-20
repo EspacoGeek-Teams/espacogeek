7d34f9d7b5084d47aa1fd648b4c17de027e9cc0b
AbigailPedreira <abigailpedreira6@gmail.com>
2024-08-20 18:29:02 -0300

    build: Base page recoverPassword

---
+++ frontend/src/containers/recoverPassword/RecoverPassword.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import TopBar from "../../components/topBar/TopBar";
+import { Form } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+
+export default function RecoverPassword() {
+
+
+    return (
+        <>
+            <TopBar />
+
+
+            <h1>Abigail</h1>
+        </>
+    );
+}
