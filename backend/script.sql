15a67d705ac1330d819b68c12090c7f065ccfe18
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-09-23 23:54:09 -0300

    feat: add more genres

--- backend/script.sql
+++ backend/script.sql
@@ -62,7 +62,28 @@ VALUES
     (21, 'News'),
     (22, 'Reality'),
     (23, 'Soap'),
-    (24, 'Talk');
+    (24, 'Talk'),
+    (25, 'Pinball'),
+    (46, 'Quiz/Trivia'),
+    (27, 'Indie'),
+    (28, 'Arcade'),
+    (29, 'Card & Board Game'),
+    (30, 'MOBA'),
+    (31, 'Point-and-click'),
+    (32, 'Fighting'),
+    (33, 'Shooter'),
+    (34, 'Music'),
+    (35, 'Platform'),
+    (36, 'Puzzle'),
+    (37, 'Racing'),
+    (38, 'Real Time Strategy (RTS)'),
+    (39, 'Role-playing (RPG)'),
+    (40, 'Simulator'),
+    (41, 'Sport'),
+    (42, 'Strategy'),
+    (43, 'Turn-based strategy (TBS)'),
+    (44, 'Tactical'),
+    (45, "Hack and slash/Beat 'em up");
 
 INSERT INTO
 espacogeekdb.api_keys
