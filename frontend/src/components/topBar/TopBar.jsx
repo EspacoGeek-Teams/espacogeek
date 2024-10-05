bdb3812b50ff7835d82ca9a81aa0db75c3f6876d
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-10-05 14:47:26 -0300

    feat: migrating navbar to primereact

--- frontend/src/components/topBar/TopBar.jsx
+++ frontend/src/components/topBar/TopBar.jsx
@@ -6,6 +6,9 @@ import { Search } from "react-bootstrap-icons";
 import SearchBar from "./SearchBar";
 import SignInBar from "./SignInBar";
 import LogInBar from "./LogInBar";
+import { Toolbar } from "primereact/toolbar";
+import { Avatar } from "primereact/avatar";
+import Home from './../../containers/home/Home'
 
 function TopBar() {
     const [SearchComponent, setSearchComponent] = useState(false);
@@ -17,6 +20,22 @@ function TopBar() {
 
     const handleLogInClose = () => setLogInComponent(false);
     const handleLogInShow = () => setLogInComponent(true);
+
+    const startContent = <React.Fragment>EspaçoGeek</React.Fragment>;
+
+    const centerContent = (
+        <div className="flex flex-wrap align-items-center gap-3">
+            <button className="p-link inline-flex justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200" >
+                <i className="pi pi-home text-2xl"></i>
+            </button>
+            <button className="p-link inline-flex justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
+                <i className="pi pi-user text-2xl"></i>
+            </button>
+            <button className="p-link inline-flex justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
+                <i className="pi pi-search text-2xl"></i>
+            </button>
+        </div>
+    );
 
     return (
         <>
@@ -37,24 +56,47 @@ function TopBar() {
                         Espaço Geek
                     </Navbar.Brand>
                     <div className="d-flex align-items-center">
-                        <Button variant="link" className="group" onClick={() => setSearchComponent(true)}>
+                        <Button
+                            variant="link"
+                            className="group"
+                            onClick={() => setSearchComponent(true)}
+                        >
                             <Search
                                 color="white"
                                 className="group-hover:fill-blue-500 me-6 size-6"
                             />
                         </Button>
-                        <Button variant="primary" className="me-1" onClick={handleLogInShow}>
+                        <Button
+                            variant="primary"
+                            className="me-1"
+                            onClick={handleLogInShow}
+                        >
                             Log in
                         </Button>
-                        <Button variant="outline-primary" className="group" onClick={handleSignInShow}>
+                        <Button
+                            variant="outline-primary"
+                            className="group"
+                            onClick={handleSignInShow}
+                        >
                             Sign in
                         </Button>
                     </div>
                 </div>
             </Navbar>
-            {SearchComponent&&<SearchBar/>}
-            {SignInComponent&&<SignInBar show={SignInComponent} handleClose={handleSignInClose} />}
-            {LogInComponent&&<LogInBar show={LogInComponent} handleClose={handleLogInClose} />}
+
+            {SearchComponent && <SearchBar />}
+            {SignInComponent && (
+                <SignInBar
+                    show={SignInComponent}
+                    handleClose={handleSignInClose}
+                />
+            )}
+            {LogInComponent && (
+                <LogInBar
+                    show={LogInComponent}
+                    handleClose={handleLogInClose}
+                />
+            )}
         </>
     );
 }
