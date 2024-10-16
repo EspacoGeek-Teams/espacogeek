0d0875fb1df40166f67212ca2102e06db535210e
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-10-16 00:05:39 -0300

    feat: doing singinbar

--- frontend/src/components/topBar/SignInBar.jsx
+++ frontend/src/components/topBar/SignInBar.jsx
@@ -4,6 +4,10 @@ import { useMutation } from "@apollo/client";
 import singInMutation from "../apollo/schemas/mutations/signInUser";
 import { ErrorContext } from "../../contexts/ErrorContext";
 import { Dialog } from "primereact/dialog";
+import { Form, Formik } from "formik";
+import { InputText } from "primereact/inputtext";
+import { FloatLabel } from "primereact/floatlabel";
+import { Button } from "primereact/button";
 
 // eslint-disable-next-line react/prop-types
 function SignIn({ show, handleClose }) {
@@ -15,15 +19,13 @@ function SignIn({ show, handleClose }) {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
-    let handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (password === confirmPassword) {
+    function handleSubmit(values) {
+        if (values.password === values.confirmPassword) {
             newUser({
                 variables: {
-                    username: username,
-                    email: email,
-                    password: password,
+                    username: values.username,
+                    email: values.email,
+                    password: values.password,
                 },
             })
                 .then((data) => {
@@ -50,9 +52,116 @@ function SignIn({ show, handleClose }) {
                 content={({ hide }) => (
                     <div className="flex flex-col text-center items-center justify-center p-4 gap-4 bg-gradient-to-tr from-sky-700 rounded-xl">
                         <h1 className="select-none p-3 font-bold">EG</h1>
-                        
+                        <Formik
+                            initialValues={{
+                                email: "",
+                                password: "",
+                                confirmPassword: "",
+                                username: "",
+                            }}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setTimeout(() => {
+                                    handleSubmit(values);
+                                    setSubmitting(false);
+                                }, 400);
+                            }}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                                <Form
+                                    onSubmit={handleSubmit}
+                                    className="flex gap-7 flex-col"
+                                >
+                                    <FloatLabel>
+                                        <InputText
+                                            type="email"
+                                            name="email"
+                                            required={false}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="bg-white-alpha-20 border-none p-3 text-primary-50"
+                                            value={values.email}
+                                        />
+                                        {errors.email &&
+                                            touched.email &&
+                                            errors.email}
+                                        <label htmlFor="email">Email</label>
+                                    </FloatLabel>
+                                    <FloatLabel>
+                                        <InputText
+                                            type="username"
+                                            name="username"
+                                            required={false}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="bg-white-alpha-20 border-none p-3 text-primary-50"
+                                            value={values.username}
+                                        />
+                                        {errors.username &&
+                                            touched.username &&
+                                            errors.username}
+                                        <label htmlFor="username">Username</label>
+                                    </FloatLabel>
+                                    <FloatLabel>
+                                        <InputText
+                                            type="password"
+                                            name="password"
+                                            required={false}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="bg-white-alpha-20 border-none p-3 text-primary-50"
+                                            value={values.password}
+                                        />
+                                        {errors.password &&
+                                            touched.password &&
+                                            errors.password}
+                                        <label htmlFor="password">Password</label>
+                                    </FloatLabel>
+                                    <FloatLabel>
+                                        <InputText
+                                            type="password"
+                                            name="confirmPassword"
+                                            required={false}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            className="bg-white-alpha-20 border-none p-3 text-primary-50"
+                                            value={values.confirmPassword}
+                                        />
+                                        {errors.confirmPassword &&
+                                            touched.confirmPassword &&
+                                            errors.confirmPassword}
+                                        <label htmlFor="username">Confirm Password</label>
+                                    </FloatLabel>
+                                    <div className="flex align-items-center gap-2">
+                                        <Button
+                                            label="Cancel"
+                                            disabled={isSubmitting}
+                                            onClick={(e) => hide(e)}
+                                            text
+                                            className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"
+                                        />
+                                        <Button
+                                            label="Sing In"
+                                            type="submit"
+                                            disabled={isSubmitting}
+                                            onClick={(e) => hide(e)}
+                                            text
+                                            className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"
+                                        />
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
-            )} />
+                )}
+            />
 
             {/* <Modal show={show} onHide={handleClose} centered>
 
