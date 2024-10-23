a833f7e8395a72a45dd5c384c03a37a84aada833
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-10-23 19:40:35 -0300

    feat: doing search

--- frontend/src/components/topBar/SearchBar.jsx
+++ frontend/src/components/topBar/SearchBar.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from '@apollo/client';
 import searchQuery from '../apollo/schemas/queries/tvserieSearch';
+import { InputText } from "primereact/inputtext";
 
 function SearchBar() {
     const { loading, error, data } = useQuery(searchQuery, { variables: { name: "Stranger Things" } });
+    const [value, setValue] = useState('');
 
-    // Test of GraphQL, doc: https://www.apollographql.com/docs/react/get-started#step-5-fetch-data-with-usequery
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
+    async function handleSubmit(values) {
+
+    }
+
+    // // Test of GraphQL, doc: https://www.apollographql.com/docs/react/get-started#step-5-fetch-data-with-usequery
+    // useEffect(() => {
+    //     console.log(data);
+    // }, [data]);
 
     if (loading) return 'Submitting...';
     if (error) return `Submission error! ${error.message}`;
@@ -16,6 +22,11 @@ function SearchBar() {
     return (
         <>
             <div className="container absolute z-50">
+                {/* Do a InputText from primereact to search using useQuery from Apollo when type */}
+                <InputText
+                    type="search"
+                    placeholder="Search"
+                    value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
             <div className="absolute z-40 w-screen h-screen right-0 top-0 backdrop-blur-sm bg-blue-900 bg-opacity-10"></div>
         </>
