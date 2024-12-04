187a12bd933c1fee663c444301039dab9b4985f1
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-12-04 00:40:36 -0300

    organizing project

--- frontend/src/components/Apollo/Client.jsx
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const clientAPI = new ApolloClient({
-    uri: window.location.href.split(":")[0].concat(":").concat(window.location.href.split(":")[1].concat(":8080")).concat("/api"),
-    cache: new InMemoryCache(),
-});
-
-export default clientAPI;
